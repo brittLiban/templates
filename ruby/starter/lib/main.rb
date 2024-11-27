84fd28d149ceac181c59251263728a8b458941ad
brittLiban <britt.liban@student.greenriver.edu>
2024-11-26 21:24:18 -0800

    fixing main.rb in Ruby starter

--- ruby/starter/lib/main.rb
+++ ruby/starter/lib/main.rb
@@ -4,14 +4,14 @@ require 'dotenv/load'
 # This Appwrite function will be executed every time your function is triggered
 def main(context)
   # grabbing api key for later
-  apiKey = ENV['API_KEY']
+  
   # You can use the Appwrite SDK to interact with other services
   # For this example, we're using the Users service
   client = Appwrite::Client.new
   client
-  api_key = "standard_adb65471663d9ed9059fae3f3ade11319b5047720325f69118374f48ff3cb60c2ac38dff0656304887dd8139a69827566c43409a43fee68726a1441fcbb10e2c02a7c846db7d6797a47103d1635b9e2e28b8b6599ef32cfc8c1e32cedf982167d34798958e895904f173b6fcbaf2e30861fe5cf8a58a1fbac5f0bf8984c8b831"
-  appwrite_endpoint = "https://cloud.appwrite.io/v1"
-  appwrite_project_id = "6744021a002dff4af8c7"
+  endpoint = ENV['APPWRITE_ENDPOINT']
+  project_id = ENV['APPWRITE_PROJECT_ID']
+  api_key = ENV['APPWRITE_API_KEY']
   
   users = Appwrite::Users.new(client)
 
