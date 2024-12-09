cbdd0128dbb51aea27c9e7ed758a36ed2e0e0b9a
brittLiban <britt.liban@student.greenriver.edu>
2024-12-08 21:32:29 -0800

    ok

--- ruby/email-contact-form/lib/main.rb
+++ ruby/email-contact-form/lib/main.rb
@@ -6,7 +6,19 @@ def main(context)
   req = context.req
   method = req.method
   headers = req.headers
-  params = req.payload || {}
+  
+  # Parse request parameters based on method and content type
+  params = if method == 'POST'
+    if headers['content-type']&.include?('application/json')
+      JSON.parse(req.body_raw || '{}')
+    else
+      # Parse form data
+      body = req.body_raw || ''
+      URI.decode_www_form(body).to_h
+    end
+  else
+    req.query
+  end
 
   response = RequestHandler.handle(method, headers, params)
   
